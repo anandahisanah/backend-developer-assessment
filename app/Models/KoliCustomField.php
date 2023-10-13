511f8fcc5a7589180b723735d1317e06a8d5fef4
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-10-13 22:05:04 +0800

    feat: delete namespace not used

--- app/Models/KoliCustomField.php
+++ app/Models/KoliCustomField.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use App\Observers\UuidObserver;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class KoliCustomField extends Model
 {
