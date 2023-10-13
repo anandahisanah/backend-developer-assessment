1607bb7da2254156304756ccc9e3bc202164dd20
Ananda Nur Hisanah <nurhisanah@gmail.com>
2023-10-13 10:44:18 +0800

    build: routes package first

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('package')->group(function() {
     Route::get('/', [PackageController::class, 'get']);
+    Route::get('/{uuid}', [PackageController::class, 'first']);
     Route::post('/', [PackageController::class, 'create']);
 });
